4b9bf8aa13fa1e388f3a6f77a41bc1166e00ccbe
ahnafy <yasin011@morris.umn.edu>
2018-09-25 00:55:11 -0600

    add function to create bin and check if it is full

--- src/Bin.java
+++ src/Bin.java
@@ -5,19 +5,23 @@ public class bin {
     public int currentFill;
 
     public bin(int size){
-
+        this.size = size;
+        currentFill = 0;
     }
 
     // Check if the bin is full
     public boolean isFull(){
-
+        if(currentFill == size){
+            return true;
+        }
+        else{
+            return false;
         }
     }
+    // Increment how full the bin is by the number passed in with Avery
 
-    // Increment how full the bin is by the number passed in
 
-
-    // Fills the bin as much as possible and prints it
+    // Fills the bin as much as possible and print it with avery
     public static void fillBin(ArrayList arr, bin bin) {
 
             }
